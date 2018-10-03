5c8f7f7bebc9d0b825eb12fa6bc4be74fea5fe1e
Jørgen Hanssen <jorkiz97@gmail.com>
2018-10-03 19:13:31 +0200

    Added comments and refactoring

--- src/features/session/index.js
+++ src/features/session/index.js
@@ -1,18 +1,18 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
-import Login from './Login/index';
-import NotFound from './404/index';
-
 import { sagas, actions } from './actions';
 import reducer from './reducer';
+
+// Components
+import Login from './Login/index';
+import NotFound from './404/index';
 
 
 
 const mapDispatchToProps = {
   fetchUser: actions.fetchUser
 }
-
 function mapStateToProps(state) {
   const { session } = state;
   return {
@@ -20,14 +20,11 @@ function mapStateToProps(state) {
   }
 }
 
-
-
 // Components
 export default {
   Login: connect(mapStateToProps, mapDispatchToProps)(Login),
   NotFound
 };
-
 
 export {
   actions,
