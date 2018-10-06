ade5f8db545d628c5449e41482cfa2080c76a5a4
Jørgen Hanssen <jorkiz97@gmail.com>
2018-10-07 01:54:16 +0200

    Removed redundant imports

--- src/features/user/settings/View/index.js
+++ src/features/user/settings/View/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 
 import TextPanel from '../components/TextPanel';
 import PasswordPanel from '../components/PasswordPanel';
--- src/features/user/settings/components/PasswordPanel/index.js
+++ src/features/user/settings/components/PasswordPanel/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
