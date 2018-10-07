b0ebfac61056fbef3a8e75f37f49620c86d55677
Jørgen Hanssen <jorkiz97@gmail.com>
2018-10-07 17:48:53 +0200

    Made api server mocking async with delay (just for mocking)

--- src/common/http/api/API.js
+++ src/common/http/api/API.js
@@ -2,7 +2,7 @@ export async function fetchUser() {
     return new Promise((resolve, reject) => {
        setTimeout(() => {
          resolve({name: "Morten"});
-       }, 2000);
+       }, 400);
      }).then(data => data)
   }
 
@@ -34,59 +34,64 @@ export async function fetchAllCategories() {
       {
        title: "Programming Humor",
       },
-    ]), 2000);
+    ]), 1000);
   }).then(data => data)
 }
 
 export const fetchTopics = (category) => {
     console.log("Fetch topics by category =", category);
-    return [
-      {
-        title: "Topic title 1",
-        description: "Long question for forum. Lorem Ipsum dolor sit amet etc..... ",
-        owner: "bjarte",
-        date: "12.12.2018",
-        likes: "1",
-        category: "progamming",
-      },
-      {
-        title: "Topic 2",
-        description: "Long question for forum. Lorem Ipsum dolor sit amet etc..... ",
-        owner: "jørgen",
-        date: "1.12.2018",
-        likes: "12",
-        category: "progamming",
-      },
-    ]
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve([
+        {
+          title: "Topic title 1",
+          description: "Long question for forum. Lorem Ipsum dolor sit amet etc..... ",
+          owner: "bjarte",
+          date: "12.12.2018",
+          likes: "1",
+          category: "progamming",
+        },
+        {
+          title: "Topic 2",
+          description: "Long question for forum. Lorem Ipsum dolor sit amet etc..... ",
+          owner: "jørgen",
+          date: "1.12.2018",
+          likes: "12",
+          category: "progamming",
+        },
+      ]), 700);
+    }).then(data => data)
   }
 
 export const fetchTopic = (topic) => {
     console.log("Fetch topic from server with topic =", topic);
-    return {
-        title: "Long title for forum. lorem lorem lorem etc",
-        description: "Long question for forum. Lorem Ipsum dolor sit amet etc..... ",
-        owner: "morten",
-        date: "12.12.2018",
-        replies: [
-            {
-                owner: "jørgen",
-                date: "12.02.2018",
-                text: "Reply because lorem ipsum blah blah blah",
-                likes: "23",
-            },
-            {
-                owner: "bjarte",
-                date: "12.02.2018",
-                text: "Reply because lorem ipsum blah blah blah",
-                likes: "23",
-            },
-        ],
-    };
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve({
+          title: "Long title for forum. lorem lorem lorem etc",
+          description: "Long question for forum. Lorem Ipsum dolor sit amet etc..... ",
+          owner: "morten",
+          date: "12.12.2018",
+          replies: [
+              {
+                  owner: "jørgen",
+                  date: "12.02.2018",
+                  text: "Reply because lorem ipsum blah blah blah",
+                  likes: "23",
+              },
+              {
+                  owner: "bjarte",
+                  date: "12.02.2018",
+                  text: "Reply because lorem ipsum blah blah blah",
+                  likes: "23",
+              },
+          ],
+      }), 400);
+    }).then(data => data)
 }
 
 
 export const search = (query) => {
-    return {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({
       categories: [
         {
           name: "JavaScript",
@@ -115,30 +120,37 @@ export const search = (query) => {
           category: "progamming",
         }
       ]
-    }
+    }), 1000);
+  }).then(data => data)
 }
 
 
 export const fetchSettings = (uId) => {
-  return {
-    username: "morten",
-    email: "morten@morten.no",
-    profilePicture: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/05/Wyvern-programming-languages-in-one.jpg",
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({
+      username: "morten",
+      email: "morten@morten.no",
+      profilePicture: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/05/Wyvern-programming-languages-in-one.jpg",
+    }), 100);
+  }).then(data => data)
 }
 
 export const changeUsername = (username) => {
-  return {
-    username,
-    email: "morten@morten.no",
-    profilePicture: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/05/Wyvern-programming-languages-in-one.jpg",
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({
+      username,
+      email: "morten@morten.no",
+      profilePicture: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/05/Wyvern-programming-languages-in-one.jpg",
+    }), 100);
+  }).then(data => data)
 }
 
 export const changeEmail = (email) => {
-  return {
-    username: "morten",
-    email,
-    profilePicture: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/05/Wyvern-programming-languages-in-one.jpg",
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({
+      username: "morten",
+      email,
+      profilePicture: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/05/Wyvern-programming-languages-in-one.jpg",
+    }), 100);
+  }).then(data => data)
 }
